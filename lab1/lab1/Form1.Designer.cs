aa96958e2d84466a888a120f856cc39114577053
radRares1 <rad.rares1@gmail.com>
2020-03-29 22:03:14 +0300

    added lab1

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridStudents = new System.Windows.Forms.DataGridView();
+            this.dataGridCars = new System.Windows.Forms.DataGridView();
+            this.updateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridStudents
+            // 
+            this.dataGridStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridStudents.Location = new System.Drawing.Point(12, 12);
+            this.dataGridStudents.Name = "dataGridStudents";
+            this.dataGridStudents.Size = new System.Drawing.Size(240, 150);
+            this.dataGridStudents.TabIndex = 0;
+            // 
+            // dataGridCars
+            // 
+            this.dataGridCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCars.Location = new System.Drawing.Point(267, 12);
+            this.dataGridCars.Name = "dataGridCars";
+            this.dataGridCars.Size = new System.Drawing.Size(240, 150);
+            this.dataGridCars.TabIndex = 1;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(229, 202);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Update DB";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 261);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.dataGridCars);
+            this.Controls.Add(this.dataGridStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCars)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridStudents;
+        private System.Windows.Forms.DataGridView dataGridCars;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ lab1/lab1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection conn;
+        private SqlDataAdapter carAdapter, studentAdapter;
+        private DataSet dataSet;
+        private SqlCommandBuilder builder;
+        private BindingSource carSource, studentSource;
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            this.carAdapter.Update(dataSet.Tables["Car"]);
+            this.carAdapter.Update(dataSet.Tables["Student"]);
+            dataGridCars.Refresh();
+            dataGridStudents.Refresh();
+            MessageBox.Show("Records Updated");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            initData();
+            
+        }
+
+        public void initData()
+        {
+            
+            this.conn = new SqlConnection("Data Source = RARES\\SQLEXPRESS ; Initial Catalog=drivingSchool; Integrated Security = true");
+            this.dataSet = new DataSet();
+            this.carAdapter = new SqlDataAdapter("SELECT * FROM Car", this.conn);
+            this.studentAdapter = new SqlDataAdapter("Select * FROM Student", this.conn);
+            this.builder = new SqlCommandBuilder(carAdapter);
+
+            carAdapter.Fill(dataSet, "Car");
+            studentAdapter.Fill(dataSet, "Student");
+
+            DataRelation fk = new DataRelation("carFk", dataSet.Tables["Student"].Columns["carID"], dataSet.Tables["Car"].Columns["carID"]);
+
+            dataSet.Relations.Add(fk);
+
+            studentSource = new BindingSource();
+            studentSource.DataSource = dataSet;
+            studentSource.DataMember = "Student";
+
+            carSource = new BindingSource();
+            carSource.DataSource = studentSource;
+            carSource.DataMember = "carFk";
+
+            dataGridStudents.DataSource = studentSource;
+            dataGridCars.DataSource = carSource;
+
+        }
+
+    }
+}
